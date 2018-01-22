56af8fcecaf2bda5625b3b95139a938c8a7d9721
callmekurisu <callmekurisu@gmail.comx>
2018-01-22 00:50:11 +0000

    add link to htlc.me

--- test.js
+++ test.js
@@ -6,6 +6,13 @@ var credentials = grpc.credentials.createSsl(lndCert);
 var lnrpcDescriptor = grpc.load("rpc.proto");
 var lnrpc = lnrpcDescriptor.lnrpc;
 var lightning = new lnrpc.Lightning('localhost:10009', credentials); 
+
+var QRCode = require('qrcode')
+ 
+QRCode.toDataURL('test pay_req', function (err, url) {
+  console.log(url)
+});
+
 call = lightning.walletBalance({ 
     witness_only: false,
   }, function(err, response) {
