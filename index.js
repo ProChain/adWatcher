096dfaac8624e12e43e556def2f2be946d002ef1
callmekurisu <callmekurisu@gmail.comx>
2018-03-24 09:14:55 +0000

    SSL verification and LND mainnet migration complete

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//testing nodemon 04FEB18 at 1614
+//testing nodemon 22MAR18 at 0221
 var grpc = require('grpc');
 var fs = require('fs');
 var express = require('express');
@@ -12,66 +12,96 @@ var html = (fs.readFileSync('main.html').toString());
 var reqPage = (fs.readFileSync('request.html').toString());
 var sleep = require('sleep');
 var moment = require('moment'); 
+var cmd = require("node-cmd");
+var http = require('http');
 var https = require('https');
 
 
+var key = fs.readFileSync('adwatcher.key');
+var cert = fs.readFileSync( 'primary.crt' );
+var ca = fs.readFileSync( 'intermediate.crt' );
+
+var options = {
+  key: key,
+  cert: cert,
+  ca: ca
+};
+
+var http = require('http');
+http.createServer(app).listen(80);
+
+var https = require('https');
+https.createServer(options, app).listen(443);
+
+var forceSsl = require('express-force-ssl');
+
+app.use(forceSsl);
+
+//app.set('port', (process.env.PORT || 7777));
+
 require('events').EventEmitter.defaultMaxListeners = Infinity;
 
-
-app.set('port', (process.env.PORT || 7777));
+//ssl verification
+app.get('/.well-known/acme-challenge/xrrR-rYkdlbr0NhgIvs9mKKKNF5UkYGYEHSoKocc4HE',function (req, res){
+    res.send("xrrR-rYkdlbr0NhgIvs9mKKKNF5UkYGYEHSoKocc4HE.PThdngPVfGwq_KDu37deNclIbvsyZ4Q2OExU66Y7Jq4")
+    
+});
 
 app.get('/',function (req, res) {
     console.log("Home page loaded.")
     
-     //create invoice to allow skipping
-     call = lightning.addInvoice({ 
-        memo: "adwatcher",
-        value: 50,
-        }, function(err, response) {
-            
-            console.log('AddInvoice: ' + response.payment_request);
-            
+    //generate invoice for donations
+    cmd.get(
+        'cd ~/gocode/bin/ && ./lncli addinvoice 50',
+        function(err, data, stderr){
+            var add = JSON.parse(data)
+            console.log('Donation pay_req: ',add.pay_req)
+
             //display newly generated invoices everytime the page loads with qr code(still working on qr code gerneration)
-            res.send(html+''+ '<br><h4 id="note1">Please pay 50 sat invoice to donate, then click "support" to verify.</h4><p id="invoice">'+response.payment_request+'</p><a href="http://adwatcher.hopto.org:7777/skip/' +response.payment_request + '/"'+'><img id="support" src="https://pre00.deviantart.net/b38e/th/pre/i/2015/181/f/3/youtube_support_button__donation_button__by_koolgoldfinch-d8zf3if.png"></img></a><!--Hide the pay button until user watches for a minute --><script>function readyToPay() {$("#paid").show("slow");};$("#paid").hide(); window.setTimeout(readyToPay, 300000);</script>');
-     });
-});
+            res.send(html+''+ '<br><h4 id="note1">Please pay 50 sat invoice to donate, then click "support" to verify.</h4><p id="invoice">'+add.pay_req+'</p><a href="https://adwatcher.hopto.org/skip/' +add.pay_req + '/"'+'><img id="support" src="https://pre00.deviantart.net/b38e/th/pre/i/2015/181/f/3/youtube_support_button__donation_button__by_koolgoldfinch-d8zf3if.png"></img></a><!--Hide the pay button until user watches for a minute --><script>function readyToPay() {$("#paid").show("slow");};$("#paid").hide(); window.setTimeout(readyToPay, 300000);</script>');
+       }); 
+    });
   
  app.get('/nodeInfo', function (req, res){
-   //generate current walletBalance
-    call = lightning.walletBalance({
-        witness_only: false,
-    }, function(err, response) {
-          console.log('WalletBalance: ' + response.total_balance);
-          var balance = response.total_balance;
    
-         //generate channel balance
-     call = lightning.channelBalance({}, function(err, response) {
-            console.log('ChannelBalance: ' + response.balance);
-            var chanBalance = response.balance
-  
-            //get number of peers
-  call = lightning.listPeers({}, function(err, response) {
-    console.log('Peer Count: ' + '"' + response.peers.length + '"');
-    var peeps = response.peers.length;
+    //generate channel balance          
+    cmd.get(
+        'cd ~/gocode/bin/ && ./lncli channelbalance',
+        function(err, data, stderr){
+            var cb = JSON.parse(data)
+            console.log('Channel Balnce: ',cb.balance);
 
-    res.send(reqPage+'<br></h4 id="note1"><br><h4>Peers: ' + peeps  + '<br><h4 id="note1">Channel Balance: '+ chanBalance + '</h4><br><input type="button" value="Go Back" onclick="goBack()" class="btn-primary btn"></body></div> <iframe id="video" width="1200" height="600" src="http://rpubs.com/callmekurisu/lnd"></iframe>');
+            var chanBalance = cb.balance
+  
+    //get number of peers
+    cmd.get(
+        'cd ~/gocode/bin/ && ./lncli listpeers',
+        function(err, data, stderr){
+            var lp = JSON.parse(data)
+            console.log('Peers: ',lp.peers.length);
+
+            var peeps = lp.peers.length
+
+
+    res.send(reqPage+'<br></h4 id="note1"><br><h4>Peers: ' + peeps  + '<br><h4 id="note1">Channel Balance: '+ chanBalance + '</h4><br><input type="button" value="Go Back" onclick="goBack()" class="btn-primary btn"></body></div> <iframe id="video" width="1200" height="600" src="https://lnmainnet.gaben.win/"></iframe>');
      });
     });
   });
-});
    
  //handle skip ad or donate request by checking for payment
 app.get('/skip/:Invoice/',function (req, res) {
     var data = req.params;
     var invoice = data.Invoice;
+    
+     cmd.get(
+        'cd ~/gocode/bin/ && ./lncli listinvoices',
+        function(err, data, stderr){
+            var li = JSON.parse(data)
+            console.log('Invoices: ', li)
 
-    call = lightning.listInvoices({
-        pending_only: false,
-    }, function(err, response) {
-
-        for (i=0;i<response.invoices.length;i++){
-            var rpr = response.invoices[i]["payment_request"];
-            var settled = response.invoices[i]["settled"];
+        for (i=0;i<li.length;i++){
+            var rpr = li.invoices[i]["payment_request"];
+            var settled = li.invoices[i]["settled"];
             console.log(rpr + ' ' + settled);
             if (invoice == rpr && settled == true) {
                 console.log('Paid invoice: ', rpr);
@@ -81,9 +111,8 @@ app.get('/skip/:Invoice/',function (req, res) {
                 res.send(reqPage + '<h2 id="note1">Invoice not paid! (T_T)</h2><script>window.setTimeout(function reload() {window.location.assign("https://en.wikipedia.org/wiki/Selfishness")}, 3000); </script>');
             }
           }
-
-      })
-    });
+        });
+      });
 
 //end skip ad handling
 
@@ -99,58 +128,35 @@ app.get('/request/:Payment/',function (req, res) {
     var pay_req = data.Payment;
     console.log(pay_req);
     
-  
-    //send 1 satoshi payment if there is a valid invoice
-    call = lightning.sendPaymentSync({ 
-    amt: 5,
-    payment_request: pay_req,
-  }, function(err, response) {
-    if (!err) {
-        console.log('SendPaymentSync: ' + response.payment_route); 
-        
-         //give the API a few seconds to fetch valid payment info
-        sleep.sleep(3);
-        //send last payment data from listpayments array
-        call = lightning.listPayments({}, function(err, response) {
-            var recent = response.payments.length-1;
-            var hash = response.payments[recent].payment_hash 
-            var date = response.payments[recent].creation_date;
-            var ts = moment.unix(date).format("lll");
-            console.log('ListPayments: ' + hash);
-            //pass listpayments API data to web app
-            res.send(reqPage+'<br><h4 id="note1">Sent five satoshis!</h4><br><h4>Payment Hash: '+ hash + '<br><h4 id="note1">Timestamp: '+ ts  +  '</h4><br><input id="back" type="button" value="Go Back" onclick="goBack()" class="btn-primary btn"></body></div>');
-        
-    });
+    cmd.get( 
+       'cd ~/gocode/bin/ && ./lncli sendpayment --amt 5 --pay_req='+pay_req,
+            function(err, data, stderr){
+            var sp = JSON.parse(data)
+                 
+            //send 5 satoshi payment if there is a valid invoice
+            console.log('Send Payment: ' + sp.payment_preimage); 
+            
+            cmd.get( 
+            'cd ~/gocode/bin/ && ./lncli listpayments',
+                function(err, data, stderr){
+                   var listpay = JSON.parse(data)
+                   console.log('Payments: ', listpay.payments )
+                   if (sp.payment_error = ''){ 
+                   var recent = listpay.payments.length-1;
+                   var hash = listpay.payments[recent].payment_hash
+                   var date = listpay.payments[recent].creation_date;
+                   var ts = moment.unix(date).format("lll"); 
+                   console.log('ListPayments: ' + hash);
+                   //pass listpayments API data to web app
+                   res.send(reqPage+'<br><h4 id="note1">Sent five satoshis!</h4><br><h4>Payment Hash: '+ hash + '<br><h4 id="note1">Timestamp: '+ ts  +  '</h4><br><input id="back" type="button" value="Go Back" onclick="goBack()" class="btn-primary btn"></body></div>');
     
     } else {
         console.log("Invalid payment request.");
-        console.log(err)   
-        res.send(reqPage + '<h2 id="note1">'+'"'+err+'"'+'</h2><br><input type="button" value="Go Back" onclick="goBack()" class="btn-primary btn"></body></div>');
-        
-      
-    }
-    
+        res.send(reqPage + '<h2 id="note1">'+'"'+"Could not process your request!"+'"'+'</h2><br><input type="button" value="Go Back" onclick="goBack()" class="btn-primary btn"></body></div>');          
+           } 
+    });
   });
-    
-
-    
-});
+}); 
 
 
-//hidden feature. I'm practicing building network graphs on my own
-app.get('/graph/',function (req, res) {
-    call = lightning.describeGraph({}, function(err, response) {
-    console.log('DescribeGraph: ' + response);
-    res.send(response)    
-   });
-});
 
-  //live testing
-    app.listen(7777, '0.0.0.0', function(err) {
-  console.log("Started listening on %s", app.url);
-});
-
-  /*for running on localhost
-    app.listen(5000, function(err) {
-  console.log("Started listening on port 5000...");
-});*/
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-
+//LND API not working 21MAR18 2209
 var fs = require('fs');
 var lndCert = fs.readFileSync("/home/ubuntu/.lnd/tls.cert");
 var grpc = require('grpc')
@@ -7,32 +7,22 @@ var lnrpcDescriptor = grpc.load("rpc.proto");
 var lnrpc = lnrpcDescriptor.lnrpc;
 var lightning = new lnrpc.Lightning('localhost:10009', credentials); 
 
-var QRCode = require('qrcode')
- 
-QRCode.toDataURL('test pay_req', function (err, url) {
-  console.log(url)
+var cmd = require("node-cmd")
+
+var pay_req = 'lnbc1500n1pdtvyuwpp5e50wd3xv2ru6tyfvryemgz0gf25679vnc8nwef9452mm33e3nq9sdpc2fjkzepqg9e8g6trd3jn5gzxwfjk2gzdv95kumn9wssyx6rpdehx2mrncqzys3ch8ulyjxhysuuj0lasekymc9cuk55ttm2w9xd4qnyugpwu77zvrmkk3fkjj75fgq8vlx78crmdpauvj2t7cpuvn0du465g3fqpdwhcqu8vfeh'
+
+cmd.get(
+       'cd ~/gocode/bin/ && ./lncli sendpayment --amt 5 --pay_req='+pay_req,
+            function(err, data, stderr){
+                var sp = JSON.parse(data)
+                console.log('Sent Payment', sp) 
+
 });
 
-call = lightning.walletBalance({ 
-    witness_only: false,
-  }, function(err, response) {
-    console.log('WalletBalance: ' + response.total_balance);
-    console.log(err)
-  });
-
-call = lightning.listPeers({}, function(err, response) {
- 
-    console.log('ListPeers: ' + '"'+ response.peers.length + '"');
-  });
-
-    
-     //create invoice to allow skipping
-     call = lightning.addInvoice({ 
+/*
+call = lightning.addInvoice({
         memo: "adwatcher",
-        value: 10,
+        value: 50,
         }, function(err, response) {
-            
-            console.log('AddInvoice: ' + JSON.stringify(response));
-            console.log(err)
-            //display newly generated invoices everytime the page loads with qr code
-            });
+
+            console.log('AddInvoice: ' + response.payment_request);*/
